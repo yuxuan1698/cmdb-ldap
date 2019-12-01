c6afc15acef9356f9e10857110b2d980f4dbf678
duanbaihong <duanbaihong@qq.com>
2019-12-01 21:58:16 +0800

    add

--- front/.umirc.js
+++ front/.umirc.js
@@ -91,7 +91,7 @@ export default {
   },
   proxy: {
     "/api": {
-      "target": "http://127.0.0.1:8000/api",
+      "target": "http://172.16.0.13:8000/api",
       "changeOrigin": true,
       "pathRewrite": { "^/api" : "" }
     }
